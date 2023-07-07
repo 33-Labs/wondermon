0702af1509cc6eb9ea6253195a0b7727011580b7
lanford33 <lanfordcai@outlook.com>
2023-07-07 17:57:35 +0800

    feat: init client

---
+++ client/Wondermon/Wondermon/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Wondermon
+//
+//  Created by Cai Linfeng on 2023/7/7.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .brown
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ client/Wondermon/WondermonUITests/WondermonUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  WondermonUITestsLaunchTests.swift
+//  WondermonUITests
+//
+//  Created by Cai Linfeng on 2023/7/7.
+//
+
+import XCTest
+
+final class WondermonUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
