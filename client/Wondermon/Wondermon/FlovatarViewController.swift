9e78b09eef1c348d858834331b98da5e1c52f3f3
lanford33 <lanfordcai@outlook.com>
2023-07-08 17:41:32 +0800

    feat: Speech to text

--- client/Wondermon/Wondermon/FlovatarViewController.swift
+++ client/Wondermon/Wondermon/FlovatarViewController.swift
@@ -8,8 +8,10 @@
 import UIKit
 import Flow
 import Macaw
+import Speech
+import AVFoundation
 
-class FlovatarViewController: UIViewController {
+class FlovatarViewController: UIViewController, SFSpeechRecognizerDelegate {
     
     private lazy var svgView: SVGView = {
         let node = try! SVGParser.parse(resource: "placeholder", ofType: "svg")
@@ -24,14 +26,41 @@ class FlovatarViewController: UIViewController {
 
         return view
     }()
+    
+    private let audioEngine = AVAudioEngine()
+    private let audioSession = AVAudioSession.sharedInstance()
+    private lazy var speechRecognizer: SFSpeechRecognizer = {
+        let speechRecognizer = SFSpeechRecognizer(locale: Locale(identifier: "en-US"))!
+        speechRecognizer.delegate = self
+        return speechRecognizer
+    }()
+    private var recognitionRequest: SFSpeechAudioBufferRecognitionRequest?
+    private var recognitionTask: SFSpeechRecognitionTask?
+    private var convertedText: String?
 
+    private lazy var audioButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Speak", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.setTitle("Stop", for: .focused)
+        button.setTitle("Stop", for: .highlighted)
+        button.setTitle("Stop", for: .disabled)
+        button.backgroundColor = .yellow
+        
+        button.addTarget(self, action: #selector(audioButtonTapped), for: .touchDown)
+        button.addTarget(self, action: #selector(audioButtonTapped), for: .touchUpInside)
+        button.addTarget(self, action: #selector(audioButtonTapped), for: .touchUpOutside)
+        return button
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         print("viewDidLoad")
         view.backgroundColor = .purple
-        setupImageView()
+        setupUI()
+        setupAudio()
         fetchFlovatarData()
+        
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -40,35 +69,50 @@ class FlovatarViewController: UIViewController {
 
     }
     
-    private func fetchFlovatarData() {
-        DispatchQueue.global().async {
-            Task {
-                do {
-                    let rawAddress = "0xb3f51e9437851f08"
-                    let tokenIds = try await self.fetchFlovatarIds(rawAddress: rawAddress)
-                    if (tokenIds.count > 0) {
-                        let tokenId = tokenIds[0]
-                        let svg = try await self.fetchFlovatarSvg(rawAddress: rawAddress, flovatarId: tokenId)
-                        let node = try SVGParser.parse(text: svg)
-                        
-                        DispatchQueue.main.async { [weak self] in
-                            guard let sSelf = self else { return }
-                            sSelf.svgView.node = node
-                        }
-                    }
-                } catch {
-                    print(error)
+    private func setupAudio() {
+        SFSpeechRecognizer.requestAuthorization { (authStatus) in
+            DispatchQueue.main.async {
+                if authStatus == .authorized {
+                    self.audioButton.isEnabled = true
+                } else {
+                    self.audioButton.isEnabled = false
                 }
             }
-
         }
-
+        
+        do {
+            try audioSession.setCategory(.record, mode: .default)
+        } catch {
+            print("setup audioSession failed")
+        }
+        
     }
     
-    private func setupImageView() {
+    private func fetchFlovatarData() {
+        Task {
+            do {
+                let rawAddress = "0xb3f51e9437851f08"
+                let tokenIds = try await self.fetchFlovatarIds(rawAddress: rawAddress)
+                if (tokenIds.count > 0) {
+                    let tokenId = tokenIds[0]
+                    let svg = try await self.fetchFlovatarSvg(rawAddress: rawAddress, flovatarId: tokenId)
+                    let node = try SVGParser.parse(text: svg)
+                    
+                    DispatchQueue.main.async { [weak self] in
+                        guard let sSelf = self else { return }
+                        sSelf.svgView.node = node
+                    }
+                }
+            } catch {
+                print(error)
+            }
+        }
+    }
+    
+    private func setupUI() {
         view.addSubview(imageView)
         imageView.translatesAutoresizingMaskIntoConstraints = false
-        imageView.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -50).isActive = true
+        imageView.widthAnchor.constraint(equalTo: view.safeAreaLayoutGuide.widthAnchor, constant: -50).isActive = true
         imageView.heightAnchor.constraint(equalTo: imageView.widthAnchor).isActive = true
         imageView.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -50).isActive = true
         imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
@@ -79,6 +123,13 @@ class FlovatarViewController: UIViewController {
         svgView.heightAnchor.constraint(equalTo: svgView.widthAnchor).isActive = true
         svgView.centerYAnchor.constraint(equalTo: imageView.centerYAnchor).isActive = true
         svgView.centerXAnchor.constraint(equalTo: imageView.centerXAnchor).isActive = true
+        
+        view.addSubview(audioButton)
+        audioButton.translatesAutoresizingMaskIntoConstraints = false
+        audioButton.widthAnchor.constraint(equalTo: view.safeAreaLayoutGuide.widthAnchor, constant: -60).isActive = true
+        audioButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        audioButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        audioButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20).isActive = true
     }
     
     private func fetchFlovatarSvg(rawAddress: String, flovatarId: UInt64) async throws -> String {
@@ -138,5 +189,80 @@ class FlovatarViewController: UIViewController {
           return flovatar.getSvg()
         }
         """
+    }
+    
+    @objc func audioButtonTapped(_ sender: UIButton) {
+        if audioEngine.isRunning {
+            stopRecording()
+        } else {
+            startRecording()
+        }
+    }
+    
+    private func startRecording() {
+        audioButton.isEnabled = false
+        convertedText = nil
+        if recognitionTask != nil {
+            recognitionTask?.cancel()
+            recognitionTask = nil
+        }
+        
+        recognitionRequest = SFSpeechAudioBufferRecognitionRequest()
+        guard let recognitionRequest = recognitionRequest else {
+            fatalError("Unable to create an SFSpeechAudioBufferRecognitionRequest object")
+        }
+        recognitionRequest.shouldReportPartialResults = true
+        
+        do {
+            try audioSession.setActive(true)
+            recognitionTask = speechRecognizer.recognitionTask(with: recognitionRequest, resultHandler: { [weak self] (result, error) in
+                guard let sSelf = self else { return }
+                
+                var finished = false
+                if let result = result {
+                    let recognizedText = result.bestTranscription.formattedString
+                    print("Recognized Text: \(recognizedText)")
+                    if result.isFinal {
+                        sSelf.convertedText = recognizedText
+                        print("Converted: \(sSelf.convertedText!)")
+                        finished = true
+                    }
+                }
+                
+                if let error = error {
+                    print("Recognition error: \(error)")
+                }
+                
+                if error != nil || finished {
+                    sSelf.resetRecordingStatus()
+                }
+            })
+            
+            let recordingFormat = audioEngine.inputNode.outputFormat(forBus: 0)
+            audioEngine.inputNode.installTap(onBus: 0, bufferSize: 1024, format: recordingFormat) { (buffer, _) in
+                recognitionRequest.append(buffer)
+            }
+            audioEngine.prepare()
+            try audioEngine.start()
+        } catch {
+            resetRecordingStatus()
+            print("Audio engine error: \(error)")
+        }
+    }
+    
+    private func stopRecording() {
+        resetRecordingStatus()
+    }
+    
+    private func resetRecordingStatus() {
+        try? audioSession.setActive(false)
+        audioEngine.stop()
+        recognitionRequest?.endAudio()
+        audioEngine.inputNode.removeTap(onBus: 0)
+
+        recognitionRequest = nil
+        recognitionTask = nil
+
+        audioButton.isEnabled = true
     }
 }
